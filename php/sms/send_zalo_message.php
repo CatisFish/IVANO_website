13f35f46426fa8827ae53d5cd1e05712ffc006b7
Pham Van Quoc Dai <daik2k2@gmail.com>
2024-05-20 23:56:26 +0700

    phpnew

--- php/sms/send_zalo_message.php
+++ php/sms/send_zalo_message.php
@@ -1,7 +1,5 @@
 <?php
-$accessToken = 'rTkcMDhxBKpIqwDKsVyaJDdMgLdIuI8UlBNDHBNE6I6vdjDSqjq2H8Mtx23At2j1beEa0Uh8IM-ckhaHw9L-T8V9kItLcsjAmDQO5OkZJs7B-wCEdgiePjwAhWk8osXWteAtDO3iSK2EZVydqQGbEyJnhbYQsIWhxFNUJfY36bRjpjmMk-0oIS71s0EywpbZr8VHE9sp6dF9xP9km89A2fVPZdIVWG4vYlw6Vy7AO26nYknIplyd6RIsr0RvdHDQbEZK9UZSMb7ZYB4ohDv0L-w8pKo2-WS5xQ_jSRFxPXh1hxu7mU0tV8k1tphngp5C-CQv3hgcTqlu_FylfeuCRkZ7YoIUx0LbaOoC2Z1dE1c-BDdiAqO';
-$recipientId = '2970163350482099819';
-$message = 'Hello, this is a test message from Zalo!';
+//acc
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://openapi.zalo.me/v2.0/oa/message");
