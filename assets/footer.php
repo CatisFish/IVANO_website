6ccbea8f776ea1d3edc61db1055cdb9aadc723d5
taikonn902 <taikonn09@gmail.com>
2024-05-27 14:44:08 +0700

    fixbug 27/05

---
+++ assets/footer.php
@@ -0,0 +1,88 @@
+<footer id="footer-page">
+    <div class="ft-top">
+        <a href="tel:+0886277772">0886 277 772</a>
+    </div>
+
+    <div class="ft-mid">
+        <div class="container-ft-mid">
+            <div class="ft-mid-left">
+                <img src="images/logo.png" alt="logo">
+
+                <h2>Công ty cổ phần IVANO Việt Nam</h2>
+                <p><i class="fa-solid fa-hand-point-right"></i>Địa chỉ: <span> Số 36, KDC Hàng Bàng, Phường An Khánh,
+                        Quận Ninh Kiều, TP Cần Thơ</span></p>
+                <p><i class="fa-solid fa-hand-point-right"></i>MST: 1801595978</p>
+                <p><i class="fa-solid fa-hand-point-right"></i>Điện thoại: 0886 277772 – 0856 277772</p>
+                <p><i class="fa-solid fa-hand-point-right"></i>Website: www.ivano.com.vn</p>
+                <p><i class="fa-solid fa-hand-point-right"></i>Email: ivanovietnam@gmail.com</p>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="ft-bottom">
+        <p>IVANO &copy;2024</p>
+    </div>
+</footer>
+
+<style>
+    .ft-top {
+        background: linear-gradient(to bottom right, #555e63, #849396, #c7c3c0, #e2cdaf, #ecdbbf);
+        padding: 30px 0;
+        text-align: center;
+    }
+
+    .ft-top a {
+        color: #FFF;
+        font-weight: 700;
+        font-size: 30px;
+    }
+
+    .ft-mid {
+        background: url("images/Paint-splash-black-background.jpg");
+        background-size: 100% !important;
+        background-repeat: no-repeat !important;
+        background-position: center top 20%;
+        padding: 20px 0;
+    }
+
+    .container-ft-mid {
+        width: 90%;
+        margin: 0px auto;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .ft-mid-left{
+        width: 33%;
+    }
+    .ft-mid-left img {
+        width: 150px;
+        
+    }
+
+    .ft-mid-left h2 {
+        font-weight: 600;
+        color: #FFF;
+        font-size: 17px;
+        margin-bottom: 30px;
+        line-height: 1.3;
+    }
+
+    .ft-mid-left p{
+        color: #fff;
+        margin: 10px 0;
+    }
+
+    .fa-hand-point-right{
+        margin-right: 10px;
+    }
+
+    .ft-bottom {
+        text-align: center;
+        font-weight: 600;
+        background: linear-gradient(to right, #001065 0, #008bd3 25%, #d40063 75%, #d4be00 100%);
+        color: #FFF;
+        padding: 10px 0;
+    }
+</style>
